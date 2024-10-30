371873756472d9d144abc3b2057b38a067b12a3e
João Pedro <joaope.espindola2005@gmail.com>
2024-10-29 22:06:09 -0300

    new page almost finished structure

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,12 +1,13 @@
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 import { Section } from "../../components/Section"
+import { Input } from "../../components/Input/index.jsx";
 
 import { useAuth } from "../../hooks/auth"
 
 import { Container, Content, Img, Form } from "./styles.js"
 
-import { CaretLeft, UploadSimple  } from "@phosphor-icons/react"; 
+import { CaretLeft, UploadSimple, CaretDown } from "@phosphor-icons/react"; 
 
 
 export function New(){
@@ -30,9 +31,35 @@ const { user } = useAuth()
         <label htmlFor="img">
          <UploadSimple size="24px"/>
          <span>Selecione uma imagem</span>
+
+         <input id="uploadImg" type="file"/>
         </label>
        </Img>
       </Section>
+
+      <Section title="Nome">
+        <input 
+        className="name"
+        placeholder="Ex: Salada Ceasar"
+        type="text" />
+      </Section>
+
+      <Section title="Categoria">
+        <div className="category">
+          <label htmlFor="category">
+            <select name="category" id="category">
+              <option value="">Selecionar</option>
+              <option value="">Refeição</option>
+              <option value="">Sobremesa</option>
+              <option value="">Bebida</option>
+          </select>
+
+          <CaretDown size="24px"/>
+        </label>
+        </div>
+      </Section>
+
+    
      </Form>
     </Content>
     <Footer />
--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -1,9 +1,155 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const Container = styled.div`
+ width: 100%;
+ height: 100vh;
+ display: grid;
+ grid-template-rows: 114px auto 47px;
+ grid-template-areas:
+ "header"
+ "content"
+ "footer";
+`
 
-export const Content = styled.div``
+export const Content = styled.div`
+ display: flex;
+ flex-direction: column;
+ grid-area: content;
+ padding: 15px 23px 30px;
 
-export const Form = styled.form``
+  h1 {
+  margin-top: 16px;
+  font-weight: 500;
+  font-size: 25px;  
 
-export const Img = styled.div``
+  color: ${({ theme }) => theme.COLORS.WHITE_100};
+  }
+
+ > button {
+   border: none;
+   background: none;
+   gap: 1px;
+   font-size: 16px; 
+   display: flex;
+   align-items: center;
+   
+  p {
+    color: ${({ theme }) => theme.COLORS.WHITE_100}
+  }
+ }
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  margin: 20px 0 53px;
+
+  .name {
+    width: 100%;
+    padding: 12px 22px;
+
+    color: ${({theme}) => theme.COLORS.WHITE_100};
+
+    border: none;
+    background-color: transparent;
+    border-radius: 8px;
+    
+    background-color: ${({theme}) => theme.COLORS.DARK_800};
+    
+    &:focus-within {
+    outline: 1px solid ${({ theme }) => theme.COLORS.WHITE};
+  }
+  }
+
+  ::placeholder {
+    color: ${({theme}) => theme.COLORS.WHITE_500};
+  }
+
+  input { 
+    background-color: transparent;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+
+    section {
+      width: 100%;
+    }
+
+    section input {
+      background-color: ${({ theme }) => theme.COLORS.DARK_600};
+      border-radius: 8px;
+    }
+  }
+
+  .category {
+    > label {
+      position: relative;
+    }
+
+    svg {
+      pointer-events: none;
+      position: absolute;
+      top: 0;
+      right: 18px;
+      cursor: pointer;
+      transition: filter 0.2s;
+      color: ${({theme}) => theme.COLORS.WHITE_400};
+    }
+
+    select {
+      border: none;
+      cursor: pointer;
+      width: 100%;
+      padding: 16px 16px;
+
+      appearance: none;
+      -webkit-appearance: none;
+
+
+      border-radius: 8px;
+      background-color: ${({theme}) => theme.COLORS.DARK_900};
+      color: ${({theme}) => theme.COLORS.WHITE_100};
+
+      font-weight: 400;
+      font-size: 14px;
+    }
+  }
+`
+
+export const Img = styled.div`
+  padding: 12px 22px;
+  border-radius: 8px;
+  position: relative;
+
+  background-color: ${({ theme }) => theme.COLORS.DARK_800};
+  color: ${({ theme }) => theme.COLORS.WHITE_1100};
+
+  &:focus-within {
+    outline: 1px solid ${({ theme }) => theme.COLORS.WHITE};
+  }
+
+  
+  > label {
+    display: flex;
+    gap: 8px;
+    cursor: pointer;
+
+    span {
+      font-size: 14px;
+      line-height: 24px;
+      max-width: calc(100vw - 16px);
+    }
+
+    input {
+      width: 100%;
+      position: absolute;
+      right: 0;
+      z-index: -1;
+    }
+  }
+  `
