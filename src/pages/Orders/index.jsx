e1a86efbba128306c6b85aefd1a35244dd0953b3
João Pedro <joaope.espindola2005@gmail.com>
2024-10-31 22:20:59 -0300

    feat: implements function to calculate total orders

--- src/pages/Orders/index.jsx
+++ src/pages/Orders/index.jsx
@@ -1,16 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import { Container, CloseButton } from "./styles";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
+import { Button } from "../../components/Button";
+
 import { CaretLeft } from "@phosphor-icons/react";
+
 import { api } from "../../services/api";
-import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 const BASE_URL = 'http://localhost:3333/files/';
 
 export function Orders({ cartIsOpen, cartItems = [], handleRemoveFromCart }) {
   const navigate = useNavigate();
   const [orders, setOrders] = useState([]);
+  const [total, setTotal] = useState(0); // Adiciona um estado para o total
 
   function handleBack() {
     navigate(-1);
@@ -20,19 +25,32 @@ export function Orders({ cartIsOpen, cartItems = [], handleRemoveFromCart }) {
     async function fetchOrders() {
       try {
         const response = await api.get("/orders");
-        console.log("Dados pedidos:", response.data);
+       
         const ordersWithFullUrls = response.data.map((order) => ({
           ...order,
           image: `${BASE_URL}${order.image}`,
         }));
+
         setOrders(ordersWithFullUrls);
+        calculateTotal(ordersWithFullUrls); // Chama a função para calcular o total
       } catch (error) {
         console.error("Erro ao buscar pedidos:", error);
       }
     }
     fetchOrders();
   }, []);
-  
+
+  // Função para calcular o total
+  const calculateTotal = (orders) => {
+    const totalValue = orders.reduce((acc, order) => {
+      const orderTotal = order.items.reduce((itemAcc, item) => {
+        return itemAcc + (Number(item.quantity) * Number(order.totalPrice));
+      }, 0);
+      return acc + orderTotal;
+    }, 0);
+    
+    setTotal(totalValue); // Atualiza o estado com o total calculado
+  };
 
   return (
     <Container>
@@ -63,6 +81,7 @@ export function Orders({ cartIsOpen, cartItems = [], handleRemoveFromCart }) {
             <p>Nenhum pedido encontrado.</p>
           )}
         </div>
+        <p className="total">Total: R$<span>{total.toFixed(2)}</span></p> {/* Exibe o total */}
       </section>
       <Footer />
     </Container>
