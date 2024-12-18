b0e0cb32125b8576a4babf410c74ef1dc64c1642
João Pedro Espindola <joaope.espindola2005@gmail.com>
2024-12-18 14:19:24 -0300

    applying the context in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { CartProvider } from "./hooks/cartContext.jsx";
 import { AuthProvider } from "./hooks/auth.jsx"
 import { Routes } from "./routes";
 
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <AuthProvider>
+      <CartProvider>
         <Routes />
+      </CartProvider>
       </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
